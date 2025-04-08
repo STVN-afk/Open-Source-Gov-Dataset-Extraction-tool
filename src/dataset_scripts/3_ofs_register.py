7f50bbe839534534d66ba3ce0e27427e11c0b425
Sam Horrell <shorrell1@sheffield.ac.uk>
2025-04-08 13:31:01 +0100

    Task 3

---
+++ src/dataset_scripts/3_ofs_register.py
@@ -0,0 +1,39 @@
+import os
+import requests
+import pandas as pd 
+
+
+
+def ofs_register(url, final_csv_path):
+    response = requests.get(url)
+
+    with open("ofs.xlsx", "wb") as f:
+        f.write(response.content)
+    
+    # keep_default_na=False makes sure that 'None' values don't become empty
+    excel = pd.read_excel("ofs.xlsx", sheet_name="Register", keep_default_na=False)
+    
+    # encoding stops weird encoding errors, when cells say 'None' na_rep keeps them this way
+    excel.to_csv("ofs.csv", index=False, encoding="utf-8-sig", na_rep='None')
+
+    # remove first two rows of csv, save
+    csv = pd.read_csv("ofs.csv", skiprows=2, encoding="utf-8-sig")
+    csv.to_csv(final_csv_path, index=False, encoding="utf-8-sig", na_rep='None')
+
+    # remove the unneeded excel and csv file
+    os.remove("ofs.xlsx")
+    os.remove("ofs.csv")
+
+
+
+
+
+if __name__ == "__main__":
+    
+    url = "https://register-api.officeforstudents.org.uk/api/Download/"
+
+    csv_name = "ofs_register.csv"
+    csv_dir = os.path.abspath(".csvs")
+    full_path = os.path.join(csv_dir, csv_name)
+
+    ofs_register(url, full_path)
