b600798d1a7fed5b240547d82855c7c1acc4d6bc
Sam Horrell <shorrell1@sheffield.ac.uk>
2025-04-16 15:08:13 +0100

    Ethnicity Data

--- src/dataset_scripts/nomis/Nomis.py
+++ src/dataset_scripts/nomis/Nomis.py
@@ -1,6 +1,10 @@
+import io
+import time
 import requests
 import pandas as pd
 from io import StringIO
+
+from helper_scripts import helper
 
 def Employment_Conversion():
     
@@ -18,6 +22,27 @@ def Employment_Conversion():
     }
 
     rq = requests.get(url, params=params)
+
+
+def Ethnicities_Conversion():
+    url = "https://www.nomisweb.co.uk/api/v01/dataset/NM_608_1.data.csv?date=latest&geography=1946157057...1946157404&rural_urban=0&cell=0,100,200,300,400,500&measures=20301&select=geography_name,obs_value,cell_name"
+    response = requests.get(url)
+
+    if response.status_code == 429:
+        print("Rate limit exceeded, waiting")
+        time.sleep(60)
+        return Ethnicities_Conversion()
+    elif response.status_code in [502, 504]:
+        print("Skipping because of server issues")
+        return None
+    
+    # Read CSV into DataFrame
+    df = pd.read_csv(io.StringIO(response.text))
+
+    pivot_df = df.pivot(index='GEOGRAPHY_NAME', columns='CELL_NAME', values='OBS_VALUE').reset_index()
+
+    output_path = helper.filePath("ethnicities.csv", ".csvs") 
+    pivot_df.to_csv(output_path, index=False)
 
 if __name__ == "__main__":
     Employment_Conversion()
