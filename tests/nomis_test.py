3b6350343483037844531ed51a58a8829b9979ec
Steve <negativedegre3s@gmail.com>
2025-04-23 10:09:37 +0100

    Added Docstring to my nomis functions, Added Sam's Tests for Nomis onto my test file as merge didn't work for some reason (May have issues since I cannot test)

--- tests/nomis_test.py
+++ tests/nomis_test.py
@@ -1,23 +1,24 @@
-import datetime
 import os
 from unittest.mock import patch, MagicMock
 import pandas as pd
 import helper_scripts.helper as helper
 from src.dataset_scripts.nomis import Nomis
+import datetime
 
-@patch("dataset_scripts.nomis.Nomis.requests.get") 
+
+@patch("dataset_scripts.nomis.Nomis.requests.get")
 def test_ethnicities_conversion_success(mock_get):
     mock_response = MagicMock()
     mock_response.status_code = 200
 
     input = helper.filePath("nomis_ethnicities_test_file.csv", "tests/test_datasets")
     assert os.path.exists(input), f"Input file {input} does not exist."
-    
+
     with open(input, "r") as f:
         mock_response.text = f.read()
 
     mock_get.return_value = mock_response
-    
+
     Nomis.Ethnicities_Conversion()
 
     output = helper.filePath(f"EthnicGroup_{Nomis.getDate()}.csv", ".csvs")
@@ -30,19 +31,19 @@ def test_ethnicities_conversion_success(mock_get):
 
     os.remove(output)
 
-@patch("dataset_scripts.nomis.Nomis.requests.get") 
+@patch("dataset_scripts.nomis.Nomis.requests.get")
 def test_national_averages_conversion_success(mock_get):
     mock_response = MagicMock()
     mock_response.status_code = 200
 
     input = helper.filePath("nomis_averages_test_file.csv", "tests/test_datasets")
     assert os.path.exists(input), f"Input file {input} does not exist."
-    
+
     with open(input, "r") as f:
         mock_response.text = f.read()
 
     mock_get.return_value = mock_response
-    
+
     Nomis.National_Averages_Conversion()
 
     output = helper.filePath(f"NationalAverages_{Nomis.getDate()}.csv", ".csvs")
@@ -55,19 +56,19 @@ def test_national_averages_conversion_success(mock_get):
     os.remove(output)
 
 
-@patch("dataset_scripts.nomis.Nomis.requests.get") 
+@patch("dataset_scripts.nomis.Nomis.requests.get")
 def test_population_conversion_success(mock_get):
     mock_response = MagicMock()
     mock_response.status_code = 200
 
     input = helper.filePath("nomis_population_test_file.csv", "tests/test_datasets")
     assert os.path.exists(input), f"Input file {input} does not exist."
-    
+
     with open(input, "r") as f:
         mock_response.text = f.read()
 
     mock_get.return_value = mock_response
-    
+
     Nomis.Population_Conversion()
 
     output = helper.filePath(f"Population_{Nomis.getDate()}.csv", ".csvs")
@@ -79,19 +80,19 @@ def test_population_conversion_success(mock_get):
 
     os.remove(output)
 
-@patch("dataset_scripts.nomis.Nomis.requests.get") 
+@patch("dataset_scripts.nomis.Nomis.requests.get")
 def test_population_estimation_conversion_success(mock_get):
     mock_response = MagicMock()
     mock_response.status_code = 200
 
     input = helper.filePath("nomis_population_estimate_test_file.csv", "tests/test_datasets")
     assert os.path.exists(input), f"Input file {input} does not exist."
-    
+
     with open(input, "r") as f:
         mock_response.text = f.read()
 
     mock_get.return_value = mock_response
-    
+
     Nomis.Population_Estimate_Conversion(1)
 
     output = helper.filePath(f"PopulationEstimates_Male_{Nomis.getDate()}.csv", ".csvs")
@@ -106,19 +107,19 @@ def test_population_estimation_conversion_success(mock_get):
     os.remove(output)
 
 
-@patch("dataset_scripts.nomis.Nomis.requests.get") 
+@patch("dataset_scripts.nomis.Nomis.requests.get")
 def test_unemployment_conversion_success(mock_get):
     mock_response = MagicMock()
     mock_response.status_code = 200
 
     input = helper.filePath("nomis_unemployment_test_file.csv", "tests/test_datasets")
     assert os.path.exists(input), f"Input file {input} does not exist."
-    
+
     with open(input, "r") as f:
         mock_response.text = f.read()
 
     mock_get.return_value = mock_response
-    
+
     Nomis.Unemployment_Conversion()
 
     output = helper.filePath(f"Unemployed_{Nomis.getDate()}.csv", ".csvs")
@@ -131,8 +132,7 @@ def test_unemployment_conversion_success(mock_get):
 
     os.remove(output)
 
-
-@patch("dataset_scripts.nomis.Nomis.requests.get") 
+@patch("dataset_scripts.nomis.Nomis.requests.get")
 def test_employment_conversion_success(mock_get):
     mock_response1 = MagicMock()
     mock_response2 = MagicMock()
@@ -146,7 +146,7 @@ def test_employment_conversion_success(mock_get):
     assert os.path.exists(input1), f"Input file {input1} does not exist."
     assert os.path.exists(input2), f"Input file {input2} does not exist."
 
-    
+
     with open(input1, "r") as f:
         mock_response1.text = f.read()
 
@@ -154,7 +154,7 @@ def test_employment_conversion_success(mock_get):
         mock_response2.text = f.read()
 
     mock_get.side_effect = [mock_response1, mock_response2]
-    
+
     Nomis.Employment_Conversion()
 
     output = helper.filePath(f"Employment_{Nomis.getDate()}.csv", ".csvs")
